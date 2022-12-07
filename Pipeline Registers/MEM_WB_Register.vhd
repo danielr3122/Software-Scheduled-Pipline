8004a785025fae98837e9df9fcd26461213c4580
danielr3122 <drosenhamer@gmail.com>
2022-12-06 22:33:51 -0600

    MEM/WB Complete

---
+++ Pipeline Registers/MEM_WB_Register.vhd
@@ -0,0 +1,113 @@
+-------------------------------------------------------------------------
+-- Daniel Rosenhamer
+-- Student of Electrical and Computer Engineering
+-- Iowa State University
+-------------------------------------------------------------------------
+-- MEM_WB_Register.vhd
+-------------------------------------------------------------------------
+-- DESCRIPTION: This file is an implementation of our MEM/WB Register
+-------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity IF_ID_Register is
+    generic(N   : integer := 32);
+    port(i_CLK                  : in std_logic;
+         i_RST                  : in std_logic;
+         i_MEM_PCNext           : in std_logic_vector(31 downto 0);
+         i_MEM_Halt             : in std_logic;
+         i_MEM_Write_Data_Sel   : in std_logic_vector(1 downto 0);
+         i_MEM_RegWr            : in std_logic;
+         i_MEM_Ovfl             : in std_logic;
+         i_MEM_ALUout           : in std_logic_vector(31 downto 0);
+         i_MEM_DMemOut          : in std_logic_vector(31 downto 0);
+         i_MEM_RegWrAddr        : in std_logic_vector(4 downto 0);
+         o_WB_Halt              : out std_logic;
+         o_WB_Ovfl              : out std_logic;
+         o_WB_ALUout            : out std_logic_vector(31 downto 0);
+         o_WB_Write_Data_Sel    : out std_logic_vector(1 downto 0);
+         o_WB_DMemOut           : out std_logic_vector(31 downto 0);
+         o_WB_PCNext            : out std_logic_vector(31 downto 0);
+         o_WB_RegWrAddr         : out std_logic_vector(4 downto 0);
+         o_WB_RegWr             : out std_logic);
+end IF_ID_Register;
+
+architecture structural of IF_ID_Register is
+
+    component register_N is
+        generic(N : integer := 32);
+        port(i_Clock    : in std_logic;
+             i_Reset    : in std_logic;
+             i_WriteEn  : in std_logic;
+             i_Data     : in std_logic_vector(N-1 downto 0);
+             o_Data     : out std_logic_vector(N-1 downto 0));
+    end component;
+
+    begin
+        
+        g_PCNext: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_PCNext,
+                o_Data      => o_WB_PCNext);
+        
+        g_Halt: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_Halt,
+                o_Data      => o_WB_Halt);
+        
+        g_Write_Data_Sel: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_Write_Data_Sel,
+                o_Data      => o_WB_Write_Data_Sel);
+        
+        g_RegWr: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_RegWr,
+                o_Data      => o_WB_RegWr);
+        
+        g_Ovfl: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_Ovfl,
+                o_Data      => o_WB_Ovfl);
+        
+        g_ALUout: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_ALUout,
+                o_Data      => o_WB_ALUout);
+        
+        g_DMemOut: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_DMemOut,
+                o_Data      => o_WB_DMemOut);
+        
+        g_RegWrAddr: register_N
+            port map(
+                i_Clock     => i_CLK,
+                i_Reset     => i_RST,
+                i_WriteEn   => '1',
+                i_Data      => i_MEM_RegWrAddr,
+                o_Data      => o_WB_RegWrAddr);
+
+end structural;
